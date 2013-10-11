9b3daa7ea2217849d41fb38fec42c91298f7b343
redi <reidy.sianto@ukirama.com>
2013-10-11 17:18:47 +0700

    modify header can view menu mobile

---
+++ db/migrate/20131011091942_add_image_columns_to_galleries.rb
@@ -0,0 +1,9 @@
+class AddImageColumnsToGalleries < ActiveRecord::Migration
+  def self.up
+    add_attachment :galleries, :image
+  end
+
+  def self.down
+    remove_attachment :galleries, :image
+  end
+end
